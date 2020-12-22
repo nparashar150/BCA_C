1b65b29ffbb40d7430433ebf6d55d9525985599f
nparashar150 <nparashar150@gmail.com>
2020-12-22 13:24:09 +0530

    Pushing first assignment of C

---
+++ Assignment1/Assignment1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("This is Assignment1%c",10);
+    printf("Name %c %c : %c Naman Parashar",9,9);
+    printf("%cFather's Name %c :  Why do you wanna know my Dad's Name?",10,9,9);
+    printf("%cContact %c :  9*2*2*7*7*",10,9);
+    return 0;
+}
