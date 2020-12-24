5059ad5c02402e1a2d731e40f5c346634fdfdbb5
nparashar150 <nparashar150@gmail.com>
2020-12-24 13:53:03 +0530

    Added int && char for scanf

--- 6. Input_Output/I_O.c
+++ 6. Input_Output/I_O.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int input;
-    printf("Enter an Integer.\n");
-    scanf("%d" , &input);
-    printf("\nNumber is %d", input);
+    int input; // Defining an Integer value to be asked as Input
+    char achar[40]; // Defining a Char value to be asked as Input
+    printf("Enter an Integer:"); // Printing
+    scanf("%d", &input); // Taking input from user and giving its value to print
+    printf("\nNumber is %d", input); // Printing user Input.
+    printf("\nEnter a Character:"); // Printing
+    scanf("%s", &achar); // Taking input from user and giving its value to print
+    printf("\nCharacter is %s", achar); // Printing user Input.
     return 0;
 }
