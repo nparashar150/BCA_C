4ec90d7548f9092f79f9ba10090593fbcbb94da3
nparashar150 <nparashar150@gmail.com>
2020-12-26 01:59:42 +0530

    Made Grade allocator as per Marks

--- 016. Grade_Check/Grade_Check.c
+++ 016. Grade_Check/Grade_Check.c
@@ -5,5 +5,77 @@
 int main()
 {
     printf(CYAN"Welcome to the Program.\n"RESET);
+    float a, b, c, d, e;
+    printf(CYAN"Enter the Marks of Subjects.\n"RESET);
+    printf(CYAN"1.  A    :   "RESET);
+    scanf("%f", &a);
+    printf(CYAN"\n2.  B    :   "RESET);
+    scanf("%f", &b);
+    printf(CYAN"\n3.  C    :   "RESET);
+    scanf("%f", &c);
+    printf(CYAN"\n4.  D    :   "RESET);
+    scanf("%f", &d);
+    printf(CYAN"\n5.  E    :   "RESET);
+    scanf("%f", &e);
+    if (a >= 70)
+    {
+        printf("\nYou have got First Grade in \"A\" Subject.");
+    }else if (a >= 50)
+    {
+        printf("\nYou have got Second Grade in \"A\" Subject.");
+    }else if (a >= 40)
+    {
+        printf("\nYou have got Third Grade in \"A\" Subject.");
+    }else{
+        printf("\nYou have Failed in \"A\" Subject.");
+    }
+    if (b >= 70)
+    {
+        printf("\nYou have got First Grade in \"B\" Subject.");
+    }else if (a >= 50)
+    {
+        printf("\nYou have got Second Grade in \"B\" Subject.");
+    }else if (a >= 40)
+    {
+        printf("\nYou have got Third Grade in \"B\" Subject.");
+    }else{
+        printf("\nYou have Failed in \"B\" Subject.");
+    }
+    if (c >= 70)
+    {
+        printf("\nYou have got First Grade in \"C\" Subject.");
+    }else if (c >= 50)
+    {
+        printf("\nYou have got Second Grade in \"C\" Subject.");
+    }else if (c >= 40)
+    {
+        printf("\nYou have got Third Grade in \"C\" Subject.");
+    }else{
+        printf("\nYou have Failed in \"C\" Subject.");
+    }
+    if (d >= 70)
+    {
+        printf("\nYou have got First Grade in \"D\" Subject.");
+    }else if (d >= 50)
+    {
+        printf("\nYou have got Second Grade in \"D\" Subject.");
+    }else if (d >= 40)
+    {
+        printf("\nYou have got Third Grade in \"D\" Subject.");
+    }else{
+        printf("\nYou have Failed in \"D\" Subject.");
+    }
+    if (e >= 70)
+    {
+        printf("\nYou have got First Grade in \"E\" Subject.");
+    }else if (e >= 50)
+    {
+        printf("\nYou have got Second Grade in \"E\" Subject.");
+    }else if (e >= 40)
+    {
+        printf("\nYou have got Third Grade in \"E\" Subject.");
+    }else{
+        printf("\nYou have Failed in \"E\" Subject.");
+    }
     return 0;
 }
