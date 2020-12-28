3414215e312da37451e70bc0863e5f961a3642d1
nparashar150 <nparashar150@gmail.com>
2020-12-29 00:38:04 +0530

    Made Factorial

---
+++ 021. Factorial/Factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define BOLD "\e[1;30m"
+#define RESET "\e[1;0m"
+int main()
+{
+    printf(BOLD"Welcome to the Program.\n"RESET);
+    printf(BOLD"Enter the Number you want factorial of : "RESET);
+    int a;
+    unsigned long long b = 1;
+    scanf("%d", &a);
+    for (int c = 1; c <= a; ++c)
+    {
+        b*= c;
+        printf(BOLD"\nFactorial of %d is : %llu"RESET,a ,b);
+    }
+    return 0;
+}
